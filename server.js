d7ebd221e920a39944dc70e2ab2e8fbfbf8182fd
vladiere <ruthakaza@gmail.com>
2023-01-03 08:16:25 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const port = 3000
+
+const app = express()
+const server = require('http').createServer(app)
+
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname + '/public')))
+
+io.on('connection', function (socket) {
+    socket.on('newuser', function (username) {
+        socket.broadcast.emit('update', username + ' joined the room')
+    })
+
+    socket.on('exituser', function (username) {
+        socket.broadcast.emit('update', username + ' left the room')
+    })
+
+    socket.on('chat', function (message) {
+        socket.broadcast.emit('chat', message)
+    })
+})
+server.listen(port, () => {
+    console.log('Listening on port: ' + port);
+})
